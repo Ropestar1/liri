b9ff6e4cc659cb3fa448d0e9f2d9ff5fa3e1889e
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-10 22:49:10 -0700

    working tweet call for one tweet

--- liri.js
+++ liri.js
@@ -1,15 +1,23 @@
+var keyFile = require('./keys.js');
 var Twitter = require('twitter');
- 
+
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+	consumer_key: keyFile.twitterKeys.consumer_key,
+	consumer_secret: keyFile.twitterKeys.consumer_secret,
+	access_token_key: keyFile.twitterKeys.access_token_key,
+	access_token_secret: keyFile.twitterKeys.access_token_secret
 });
  
-var params = {screen_name: 'nodejs'};
+// console.log(keyFile.twitterKeys.consumer_key);
+// console.log(keyFile.twitterKeys.consumer_secret);
+// console.log(keyFile.twitterKeys.access_token_key);
+// console.log(keyFile.twitterKeys.access_token_secret);
+
+var params = {screen_name: 'ucbe_rm'}; //ucbe_rm was nodejs originally
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
+	if (!error) {
+		// console.log(tweets);
+		console.log('Date Tweeted', tweets[0].created_at);
+		console.log('Tweet', tweets[0].text);
   }
 });
