d6327469aff7c6267c4f848b417ab98787fc62a9
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-10 22:05:32 -0700

    start up files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'BV3Uvfh8RN5gU7eFtJ8WeCPDb',
+  consumer_secret: 's5OKlMTRWt3HcnPxCHMW3u0guWR2tdJ8qMVewN6fB2AEG6IOOs',
+  access_token_key: '884545130752991234-r351TYZstgMeSAMABjUKukA4HEWjaSc',
+  access_token_secret: 'sIjIefAtkyTxeeZ3pHXjUUvUmptZAd0MEIPtt7kir7BMu',
+}
